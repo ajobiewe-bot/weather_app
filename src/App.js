8d93851ab362bbd928bc9bd4d1fa91e2433c62c3
Ana Almeida <almeida.nunes.ana@hotmail.com>
2020-09-20 17:22:27 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import axios from "axios";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello 😍</h1>
+      <h1>Hello </h1>
     </div>
   );
 }
